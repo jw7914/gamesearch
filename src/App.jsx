5199044a29cd48c9ac3c31ef2c1eac0b069bbba3
jw7914 <jw7914@nyu.edu>
2024-08-08 22:25:39 +0800

    css libraries imported

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <HashRouter>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +29,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </HashRouter>
+
   )
 }
 
